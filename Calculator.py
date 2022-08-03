4ec7619f22e73bb8ddd537c8a8249b46e5b4fec9
Faraz class="Css"/Faraz <101413350+farazSbh86@users.noreply.github.com>
2022-08-03 21:17:03 +0430

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,57 @@
+num1 = input( "adad aval :  " )
+hesab = input( " tavan mored nazar khod ra behamrah (adad tavan)X benevisid ( Ta tvan 25 benevisid )  :  " )
+
+if hesab == "2x":
+    javab = float(num1) * float(num1)
+elif hesab == "3x":
+    javab = float(num1) * float(num1) * float(num1)
+elif hesab == "4x":
+    javab = float(num1) * float(num1) * float(num1) * float(num1)
+elif hesab == "5x":
+    javab = float(num1) * float(num1) * float(num1) * float(num1) * float(num1)
+elif hesab == "6x":
+    javab = float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1)
+elif hesab == "7x":
+    javab = float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1)
+elif hesab == "8x":
+    javab = float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1)
+elif hesab == "9x":
+    javab = float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1)
+elif hesab == "10x":
+    javab = float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1)
+elif hesab == "11x":
+    javab = float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1)
+elif hesab == "12x":
+    javab = float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1)
+elif hesab == "13x":
+    javab = float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1)
+elif hesab == "14x":
+    javab = float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1)
+elif hesab == "15x":
+    javab = float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1)
+elif hesab == "16x":
+    javab = float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1)
+elif hesab == "17x":
+    javab = float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1)
+elif hesab == "18x":
+    javab = float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1)
+elif hesab == "19x":
+    javab = float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1)
+elif hesab == "20x":
+    javab = float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1)
+elif hesab == "21x":
+    javab = float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1)
+elif hesab == "22x":
+    javab = float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1)
+elif hesab == "23x":
+    javab = float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1)
+elif hesab == "24x":
+    javab = float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1)
+elif hesab == "25x":
+    javab = float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1) * float(num1)
+
+
+else:
+     print("tavane shoma gabel andarze giri baray in nist")
+
+print("javab = " , javab )
